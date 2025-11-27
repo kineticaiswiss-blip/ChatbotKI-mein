3d900635b16dda8c9e03b4448aff0555f44a704f
kineticaiswiss-blip <kineticaiswiss@gmail.com>
2025-11-27 22:38:42 +0100

    Create permissions.js

---
+++ core/permissions.js
@@ -0,0 +1,5 @@
+// core/permissions.js
+export function getUserRoleForBot({ accounts, botId, userId, channel }) {
+  // Nur Platzhalter, später echte Logik
+  return { isAdmin: false, isCustomer: false };
+}
